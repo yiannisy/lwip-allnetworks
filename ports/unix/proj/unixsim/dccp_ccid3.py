0ee4c22e1d62dce0dc1283180a351d594fcf0834
Yiannis Yiakoumis <yiannisy@stanford.edu>
2011-09-26 16:32:32 -0700

    Adding Feedback Server

--- ports/unix/proj/unixsim/dccp_ccid3.py
+++ ports/unix/proj/unixsim/dccp_ccid3.py
@@ -3,9 +3,13 @@
 import pcap
 import dpkt
 import logging
+import time
+import threading
+import SocketServer
 from optparse import OptionParser
 
 DEFAULT_IFACE='eth0'
+DEFAULT_UDP_FEEDBACK_PORT=9091
 PORT=8081
 PKT_SIZE=1024
 
@@ -18,6 +22,10 @@ weights = [1,1,1,1,0.8,0.6,0.4,0.2]
 s_intervals = []
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+def send_ploss_report(p_loss):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(p_loss, ("171.67.75.16", 9091))
 
 # maintain history of the last 8 loss events
 def add_loss_interval(loss_interval_sample):
@@ -44,31 +52,32 @@ def get_avg_rtt(avg, rtt_sample):
         return (0.9*avg + 0.1*rtt_sample)
     else:
         return rtt_sample
-
-class IfListener(object):
+    
+class IfListener(threading.Thread):
     def __init__(self, iface, is_receiver):
+        threading.Thread.__init__(self)
         self.s_a = 0
         self.rtt_avg = 0
         self.p = pcap.pcap(iface)
         self.p.setfilter("port %d" % PORT)
+        self.is_receiver = is_receiver
 #        self.setup_recv_raw_socket(iface)
+        
+    def run(self):
         dispatched = 1
-        while(dispatched == 1):
-            if (is_receiver):
-                logging.info("Running on the receiver side")
-                dispatched = self.p.dispatch(0, self.receiver_handle_packet)
-                try:
+        try:
+            while(dispatched == 1):
+                if (self.is_receiver):
+                    logging.info("Running on the receiver side")
+                    dispatched = self.p.dispatch(0, self.receiver_handle_packet)
                     self.p.loop(self.receiver_handle_packet)
-                except KeyboardInterrupt:
-                    logging.warn("Plot something here")
-            else:
-                logging.info("Running on the sender side")
-                dispatched = self.p.dispatch(0, self.sender_handle_packet)
-                try:
+                else:
+                    logging.info("Running on the sender side")
+                    dispatched = self.p.dispatch(0, self.sender_handle_packet)
                     self.p.loop(self.sender_handle_packet)
-                except KeyboardInterrupt:
-                    logging.warn("Plot something here")
-
+        except KeyboardInterrupt:
+            logging.info("Quitting Listener Thread...")
+            
     def sender_handle_packet(self, timestamp, pkt):
         tcp_hdr = dpkt.ethernet.Ethernet(pkt).data.data
         if tcp_hdr.flags & dpkt.tcp.TH_ACK and tcp_hdr.dport == PORT:
@@ -101,22 +110,41 @@ class IfListener(object):
                         interval = (_lost - self.s_a)/1024
                         add_loss_interval(interval)
                         logging.info("Lost packet detected - current p_loss estimate:%f" % get_avg_loss_prop()) 
+                        send_ploss_report(get_avg_loss_prop())
                     self.s_a = rcvd_pkts[-1][1]
                     # logging.info("Missing %d bytes (%d packets) in the between" % (_gap, _gap/PKT_SIZE))
-                
-            
             rcvd_pkts.append((tcp_hdr.seq, tcp_hdr.seq + len(tcp_hdr.data)))
             # print "Received packet of size %d" % len(tcp_hdr.data)
-
             
+            
+class FeedbackHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        data = self.recv()
+        logging.info("Received p-loss report %s" % data)
+                     
+class FeedbackServer(SocketServer.ThreadingUDPServer):
+    def __init__(self, host='localhost', port=None, handler=FeedbackHandler):
+        if port is None:
+            port = DEFAULT_UDP_FEEDBACK_PORT
+        SocketServer.ThreadingUDPServer.__init__(self, (host, port), handler)
+                                                                                 
 if __name__ == "__main__":
     usage = "usage: %prog [options] arg"
-    description = "Update firmware to remote APs"
+    description = "TCP/DCCP congestion control"
     parser = OptionParser(usage)
     parser.description = description
     parser.add_option("-r","--receiver",dest="is_receiver",action="store_true", default=False,
                       help="Script running on the receiver side (calculate p_loss instead of RTT")
 
     (options, args) = parser.parse_args()
+    
+    listener = IfListener(DEFAULT_IFACE, options.is_receiver)
+    listener.start()
 
-    IfListener(DEFAULT_IFACE, options.is_receiver)
+    if(not options.is_receiver): 
+        feedback_rcver = FeedbackServer()
+        thr_feedback = threading.Thread(target=feedback_rcver.serve_forever)
+        thr_feedback.daemon = True
+        logging.info("Starting Feedback Server Thread")
+        thr_feedback.start()
+    
